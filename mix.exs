92eb007bb9bf8dbed2277c4f7728a6399000c2de
Marcelo Gornstein <marcelog@gmail.com>
2015-04-11 17:58:59 -0300

    adding license, preparing for hex

--- mix.exs
+++ mix.exs
@@ -3,8 +3,12 @@ defmodule RFC3986.Mixfile do
 
   def project do
     [app: :ex_rfc3986,
-     version: "0.0.1",
+     name: "ex_rfc3986",
+     version: "0.1.0",
+     source_url: "https://github.com/marcelog/ex_rfc3986",
      elixir: "~> 1.1-dev",
+     description: description,
+     package: package,
      deps: deps]
   end
 
@@ -15,16 +19,28 @@ defmodule RFC3986.Mixfile do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
+  defp description do
+    """
+    An URI parser trying to be strictly compatible with RFC3986. Uses official ABNF grammar and ex_abnf as interpreter.
+    """
+  end
+
   defp deps do
-    [{:ex_abnf, "~> 0.1.0"}]
+    [
+      {:ex_abnf, "~> 0.1.0"},
+      {:earmark, "~> 0.1", only: :dev},
+      {:ex_doc, "~> 0.7", only: :dev}
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*", "priv"],
+      contributors: ["Marcelo Gornstein"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/marcelog/ex_rfc3986"
+      }
+    ]
   end
 end
