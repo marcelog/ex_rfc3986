21954a68dc83642acad5b85b8261b9e2c21df3fa
Marcelo Gornstein <marcelog@gmail.com>
2015-04-11 18:19:26 -0300

    bumping up version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RFC3986.Mixfile do
   def project do
     [app: :ex_rfc3986,
      name: "ex_rfc3986",
-     version: "0.1.0",
+     version: "0.1.1",
      source_url: "https://github.com/marcelog/ex_rfc3986",
      elixir: "~> 1.1-dev",
      description: description,
