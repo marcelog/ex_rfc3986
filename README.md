3c6e850647eacef0af051f10377dcb7d8839faf8
Marcelo Gornstein <marcelog@gmail.com>
2015-04-16 10:22:01 -0300

    bumping up version, using new ex_abnf version

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ To use it in your Mix projects, first add it as a dependency:
 
 ```elixir
 def deps do
-  [{:ex_rfc3986, "~> 0.1.2"}]
+  [{:ex_rfc3986, "~> 0.1.3"}]
 end
 ```
 Then run mix deps.get to install it.
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RFC3986.Mixfile do
   def project do
     [app: :ex_rfc3986,
      name: "ex_rfc3986",
-     version: "0.1.2",
+     version: "0.1.3",
      source_url: "https://github.com/marcelog/ex_rfc3986",
      elixir: ">= 1.0.0",
      description: description,
@@ -27,7 +27,7 @@ defmodule RFC3986.Mixfile do
 
   defp deps do
     [
-      {:ex_abnf, "~> 0.1.2"},
+      {:ex_abnf, "~> 0.1.3"},
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, "~> 0.7", only: :dev}
     ]
