134e06d08ff4f541435439c6036ef66522a4d1a9
Marcelo Gornstein <marcelog@gmail.com>
2015-10-02 11:08:20 -0300

    updating to latest hex

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule RFC3986.Mixfile do
   defp package do
     [
       files: ["lib", "mix.exs", "README*", "LICENSE*", "priv"],
-      contributors: ["Marcelo Gornstein"],
+      mantainers: ["Marcelo Gornstein"],
       licenses: ["Apache 2.0"],
       links: %{
         "GitHub" => "https://github.com/marcelog/ex_rfc3986"
